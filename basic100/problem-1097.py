dbaa938c24e78f97ba117f33c4cba0a0dd659129
joon <joonyoungr@gmail.com>
2020-11-22 16:02:23 +0900

    add: problem-1097

---
+++ basic100/problem-1097.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+
+# Problem link: https://codeup.kr/problem.php?id=1097
+
+"""
+# Input Example (19x19 grid)
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+2
+10 10
+12 12
+"""
+
+"""
+# Output Example
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+"""
+GRID_SIZE = 19
+
+def input_parser():
+    input_grid = []    
+    for i in range(GRID_SIZE):
+        row = list(map(int, input().split(" ")))
+        input_grid.append(row)
+    operation_number = int(input())
+    operations = []
+    for i in range(operation_number):
+        operation = list(map(int, input().split(" ")))
+        operations.append(operation)
+    return input_grid, operations
+
+def operate_grid(grid, operation):
+    # horizontal
+    horiz = operation[0]
+    for i in range(GRID_SIZE):
+        grid[horiz-1][i] = 1-grid[horiz-1][i]
+    # vertical
+    vert = operation[1] 
+    for i in range(GRID_SIZE):
+        grid[i][vert-1] = 1-grid[i][vert-1]
+    
+    return grid
+
+def print_grid(grid):
+    for row in grid:
+        print(" ".join(map(str, row)))
+
+def main():
+    grid, operations = input_parser()
+    for operation in operations:
+        grid = operate_grid(grid, operation)
+    print_grid(grid)
+
+if __name__ == "__main__":
+    main()
